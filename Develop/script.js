2d85eff44088400e916c58043490ad5bd0ba6450
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2020-12-30 12:38:18 -0600

    created letter generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseString = "abcdef";
-lowerCaseString.split("");
-upperCassString = lowerCaseString.toUpperCase()
+// a string of symbols
+var symbolString = "!@#$%^&*()"
+// a string of letters
+var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+// an array of individual lower case letters
+var lowerArray = lowerCaseString.split("");
+// an array of individual upper case letters
+var upperArray = lowerCaseString.toUpperCase().split("");
+console.log(upperArray)
+// create Number Array
+// create Special Array
+
+function getOptions() {
+  var length = parseInt(prompt("How long between 8 and 128?"));
+  if(isNaN(length) === true || length > 128 || length < 8) {
+    alert("not valid number");
+    return;
+  }
+  console.log(length);
+  var hasLower = confirm("lower ok");
+  var hasUpper = confirm("has upper");
+  if(hasLower === false  && hasUpper === false) {
+    alert("Please choose one valid character type");
+    return;
+  }
+  var options =  {
+    length: length,
+    hasLower: hasLower,
+    hasUpper: hasUpper
+  }
+  return options
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -15,8 +44,27 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var password1 = "KHLHLHLH" 
-  return password1
+  var options2 = getOptions();
+  console.log(options2);
+
+  var bigArray = [];
+  var results = [];
+
+  if(options2.hasLower === true) {
+    bigArray = bigArray.concat(lowerArray)
+  }
+  if(options2.hasUpper === true) {
+    bigArray = bigArray.concat(upperArray)
+  }
+
+  for(var i = 0; i < options2.length; i++) {
+      var index = Math.floor(Math.random() * bigArray.length);
+      console.log(index)
+      var digit = bigArray[index];
+      results.push(digit)
+  }
+  return results.join("")
+  
 }
 
 // Add event listener to generate button
