098d1e1c735db00cd653514cc971a1812f6c9f2f
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-01-01 21:07:12 -0600

    added validation

--- Develop/script.js
+++ Develop/script.js
@@ -27,12 +27,42 @@ function getOptions() {
   var length = parseInt(prompt("How many characters in your password (between 8 and 128?)"));
   
   var hasNumb = confirm("Would you like numbers?");
+// Number validation
+  if(hasNumb === true) {
+    alert("Your password will have numbers")
+  }
+  else {
+    alert("Your password will not have numbers")
+  }
 
   var hasSymb = confirm("Would you like special symbols?");
+// Symbol validation
+  if(hasSymb === true) {
+    alert("Your password will have symbols")
+  }
+  else {
+    alert("Your password will not have symbols")
+  }
 
   var hasLower = confirm("Would you like lowercase letters?");
+// Lowercase validation
+  if(hasLower === true) {
+    alert("Your password will have lowercase letters")
+  }
+  else {
+    alert("Your password will not have lowercase letters")
+  }
 
   var hasUpper = confirm("Would you like uppercase letters?");
+// Uppercase validation
+  if(hasUpper === true) {
+    alert("Your password will have uppercase letters")
+  }
+  else {
+    alert("Your password will not have uppercase letters")
+  }
+
+
 
   // if it is a number between 128 and 8 set the length of password
   if(isNaN(length) === true || length > 128 || length < 8) {
